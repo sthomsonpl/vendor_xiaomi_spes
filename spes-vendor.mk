8cf67ec5893b6b4b80649f867e0fa2ce7f43ac46
Jabiyeff <cebiyevanar@gmail.com>
2022-07-02 22:18:48 +0400

    spes: Import qti-telephony-common.jar
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- spes-vendor.mk
+++ spes-vendor.mk
@@ -1341,6 +1341,7 @@ PRODUCT_PACKAGES += \
     dpmapi \
     embmslibrary \
     qcrilhook \
+    qti-telephony-common \
     vendor.qti.hardware.alarm-V1.0-java \
     vendor.qti.hardware.data.iwlan-V1.0-java \
     vendor.qti.hardware.data.iwlan-V1.1-java \
